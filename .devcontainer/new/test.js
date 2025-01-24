7a2ae8d5791228c0fcb8dfdc0c97d7f15c33267b
Ishu <165940337+Ishuydv@users.noreply.github.com>
2025-01-24 21:24:21 +0530

    Update test.js

--- .devcontainer/new/test.js
+++ .devcontainer/new/test.js
@@ -1,1 +1,20 @@
 console.log("ishu..")
+
+const mySym = Symbol("key1");
+
+const jsUser = {
+    Name: "Ishu",
+    "full name": "Ishu Yadav",
+    age: 20,
+    location: "Rewari",
+    email: "ishu@mail",
+    isLoggedIn: true,
+    [mySym]: "myKeyOne"
+}
+
+jsUser.greetings = function(){
+    console.log(`hello js user ${this.Name}`)
+}
+
+console.log(jsUser.greetings())
+console.log(jsUser)
